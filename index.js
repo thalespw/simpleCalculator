5dcb4f3afbca25ed8c259e75e439fd870f6b7b8a
Thales Lourençon <109592406+thales4110@users.noreply.github.com>
2023-04-21 16:22:03 -0300

    development JS

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function outcome() {
+    let num1 = Number(document.getElementById('num-one').value) 
+    let num2 = Number(document.getElementById('num-two').value) 
+    let res = 0
+
+    if (document.getElementById('box1').checked) 
+        res = num1 + num2
+        else if (document.getElementById('box2').checked)
+            res = num1 - num2
+        else if (document.getElementById('box3').checked)
+            res = num1 * num2
+        else if (document.getElementById('box4').checked)
+            res = num1 / num2
+    
+    
+
+
+    
+    document.getElementById('resultArea').innerHTML = String(res)
+}
+
 
